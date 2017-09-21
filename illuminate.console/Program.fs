acaea10da1fc50b9eba2314425bbbee26e1d02d8
Keith La Force <klaforce@gmail.com>
2017-09-20 22:35:05 -0400

    organizing core as a lib and adding new console proj

---
+++ illuminate.console/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Illuminate
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let x = add 1 2
+    printfn "Hello, the addition is %d" x
+    0 // return an integer exit code
