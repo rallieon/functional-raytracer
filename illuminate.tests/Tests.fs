08d043a603bc47a8066d6e26f4c73254c13f8a6e
Keith LaForce <keith.laforce@levvel.io>
2017-12-29 09:39:44 -0500

    Fixing broken tests

--- illuminate.tests/Tests.fs
+++ illuminate.tests/Tests.fs
@@ -18,19 +18,19 @@ module Tests =
         let screenCoordinate = {i = 0; j = 0}
         let rasterPlane = {screenWidth = 100; screenHeight = 100; fov = 90}
         let conversion = mapScreenCoordinateToWorldCoodinate screenCoordinate rasterPlane
-        Assert.Equal(0.01, conversion.x, 2)
+        Assert.Equal(-0.99, conversion.x, 2)
         Assert.Equal(0.99, conversion.y, 2)
     [<Fact>]
     let ``can convert top right edge screen coordinate to world coordinate`` () =
         let screenCoordinate = {i = 100; j = 100}
         let rasterPlane = {screenWidth = 100; screenHeight = 100; fov = 90}
         let conversion = mapScreenCoordinateToWorldCoodinate screenCoordinate rasterPlane
-        Assert.Equal(2.03, conversion.x, 2)
+        Assert.Equal(1.01, conversion.x, 2)
         Assert.Equal(-1.01, conversion.y, 2)
     [<Fact>]
     let ``can convert middle screen coordinate to world coordinate`` () =
         let screenCoordinate = {i = 50; j = 50}
         let rasterPlane = {screenWidth = 100; screenHeight = 100; fov = 90}
         let conversion = mapScreenCoordinateToWorldCoodinate screenCoordinate rasterPlane
-        Assert.Equal(1.02, conversion.x, 2)
+        Assert.Equal(0.01, conversion.x, 2)
         Assert.Equal(-0.01, conversion.y, 2)
