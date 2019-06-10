75a9ad9a33b8afaa16e0ef3651be315855bb1087
Keith <klaforce@gmail.com>
2019-06-10 06:55:55 -0400

    Refactoring intersection functions

--- illuminate.core/RayCasting.fs
+++ illuminate.core/RayCasting.fs
@@ -3,34 +3,9 @@ open Illuminate.Types
 open Illuminate.Operators
 open Illuminate.Math
 open Illuminate.Coordinate
+open Illuminate.Intersection
 
 module Core = 
-    let intersectSphere (camera:Camera) (ray:Direction) (sphere:Sphere) = 
-        let l = worldSubWorld sphere.origin camera
-        let tca = dotProduct (l, (ray.dirX, ray.dirY, ray.dirZ))
-        let d2 = (dotProduct (l, l)) - (tca * tca)
-        let thc = sqrt (sphere.radius - d2)
-        let t0 = tca - thc;
-        let t1 = tca + thc;
-
-        //TODO Comment
-        match tca < 0., d2 > sphere.radius, t1 < 0. && t0 < 0., t1 < 0. && t0 >= 0., t0 < 0. && t1 >=0., t0 >= 0. && t1 >= 0. with
-            | true, _, _, _, _, _ -> None
-            | false, true, _, _, _, _ -> None
-            | false, false, true, _, _, _ -> None
-            | false, false, false, true, _, _ -> Some((Shape.Sphere sphere), t0)
-            | false, false, false, false, true, _ -> Some((Shape.Sphere sphere), t1)
-            | false, false, false, false, false, true -> Some((Shape.Sphere sphere), if t1 < t0 then t1 else t0)
-            | false, false, false, false, false, false -> None
-    
-    let intersectPlane (camera:Camera) (ray:Direction) (plane:Plane) =
-        Some((Shape.Plane plane), 0.)
-
-    let intersect (camera:Camera) (ray:Direction) (shape:Shape) =
-        match (shape) with
-            | Sphere sphere -> intersectSphere camera ray sphere
-            | Plane plane -> intersectPlane camera ray plane
-
     let getHitPoint ray scene =
         scene.shapes
             |> List.map (fun shape -> (intersect scene.camera ray shape))
