3d6b5329d64d08c4e4c36a9dd49f89fc9bb3fb3e
Keith <klaforce@gmail.com>
2019-06-10 20:41:17 -0400

    adding math test

--- illuminate.tests/MathTests.fs
+++ illuminate.tests/MathTests.fs
@@ -13,3 +13,10 @@ module MathTests =
         let angle = 20.
         let convert = deg2rad angle
         Assert.Equal(0.35, convert, 2)
+    [<Fact>]
+    let ``can perform dot product`` () =
+        let vec1 = (2.0,3.0,4.0)
+        let vec2 = (4.0,5.0,6.0)
+
+        let result = dotProduct (vec1, vec2)
+        Assert.Equal(47.0, result, 2)
