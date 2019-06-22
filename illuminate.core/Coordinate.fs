9c2e0a8b9adf9313ff5defb6d56a19b8fe80901a
Keith <klaforce@gmail.com>
2019-06-22 10:22:11 -0400

    fix bug in cross product-first pass at cornell box

--- illuminate.core/Coordinate.fs
+++ illuminate.core/Coordinate.fs
@@ -10,11 +10,11 @@ module Coordinate =
     let calculateIndexFromPixelCoordinate pixel scene = 
         scene.width * pixel.coordinate.j + pixel.coordinate.i
 
-    let mapScreenCoordinateToWorldCoodinate screenCoordinate viewPlane = 
-        let imageAspectRatio = float viewPlane.width / float viewPlane.height
-        let scale = tan (float viewPlane.fov * 0.5 * System.Math.PI / 180.)
-        let invWidth = 1. / float viewPlane.width
-        let invHeight = 1. / float viewPlane.height
+    let mapScreenCoordinateToWorldCoodinate screenCoordinate scene = 
+        let imageAspectRatio = float scene.width / float scene.height
+        let scale = tan (float scene.fov * 0.5 * System.Math.PI / 180.)
+        let invWidth = 1. / float scene.width
+        let invHeight = 1. / float scene.height
         let x = (2. * ((float screenCoordinate.i + 0.5) * invWidth) - 1.) * imageAspectRatio * scale
         let y = (1. - 2. * ((float screenCoordinate.j + 0.5) * invHeight)) * scale
         {x = x; y = y; z = -1.}
--- illuminate.core/SceneWriter.fs
+++ illuminate.core/SceneWriter.fs
@@ -5,12 +5,21 @@ open System.IO
 
 module SceneWriter = 
     let castByte (col:float) =
-        if col > 255. then 255uy else byte col
+            if col > 255. then 255uy else byte col
 
-    let convertPixel pixel =
-        new SkiaSharp.SKColor( (pixel.pixelColor.r |> castByte), (pixel.pixelColor.g |> castByte), (pixel.pixelColor.b |> castByte))
+    let writeScenePPM (image:Image, scene:Scene, outputPath:string) = 
+        let wr = new System.IO.StreamWriter(outputPath)
+        wr.WriteLine "P3"
+        wr.WriteLine("{0} {1}", scene.width, scene.height)  //use writeline instead of printf for performance
+        wr.WriteLine "255"
+        ignore (image |> List.map(fun pixel -> (wr.Write("{0} {1} {2} ",  (pixel.pixelColor.r |> castByte), (pixel.pixelColor.g |> castByte), (pixel.pixelColor.b |> castByte)))))
+        wr.Close()
+        0
 
     let writeScene (image:Image, scene:Scene, outputPath:string) =
+        let convertPixel pixel =
+            new SkiaSharp.SKColor( (pixel.pixelColor.r |> castByte), (pixel.pixelColor.g |> castByte), (pixel.pixelColor.b |> castByte))
+
         let bitmap = new SKBitmap(scene.width, scene.height)
         let skiaPixels = image |> List.map convertPixel
         bitmap.Pixels <- List.toArray skiaPixels
