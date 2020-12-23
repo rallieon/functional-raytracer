8dab9b7a116ac72212e2aa9495f340f396a0a71e
Keith LaForce <klaforce@gmail.com>
2020-12-23 05:42:46 -0500

    small refactor to remove unnecessary asserts

--- illuminate.tests/TransformationTests.fs
+++ illuminate.tests/TransformationTests.fs
@@ -10,33 +10,27 @@ module TransformationTests =
     let ``can translate a vector`` () =
         let vec = vector [2.0; 2.0; -4.0]
         let transformation = {translate=Some {x=1.;y=0.;z=0.}; scale=None; rotation=None}
-        let transformedVector = (transformVector transformation vec).ToArray()
-        let expected = (vector[3.0;2.0;-4.0]).ToArray()
+        let transformedVector = (transformVector transformation vec).ToSeq()
+        let expected = (vector[3.0;2.0;-4.0]).ToSeq()
 
-        Assert.Equal(expected.[0], transformedVector.[0])
-        Assert.Equal(expected.[1], transformedVector.[1])
-        Assert.Equal(expected.[2], transformedVector.[2])
+        Assert.Equal<seq<float>>(expected, transformedVector)
 
     [<Fact>]
     let ``can scale a vector`` () =
         let vec = vector [2.0; 2.0; -4.0]
         let transformation = {translate=None; scale=Some {x=2.;y=1.;z=1.}; rotation=None}
-        let transformedVector = (transformVector transformation vec).ToArray()
-        let expected = (vector[4.0;2.0;-4.0]).ToArray()
+        let transformedVector = (transformVector transformation vec).ToSeq()
+        let expected = (vector[4.0;2.0;-4.0]).ToSeq()
 
-        Assert.Equal(expected.[0], transformedVector.[0])
-        Assert.Equal(expected.[1], transformedVector.[1])
-        Assert.Equal(expected.[2], transformedVector.[2])
+        Assert.Equal<seq<float>>(expected, transformedVector)
 
 
     [<Fact>]
     let ``can scale a vector by multiple dimensions`` () =
         let vec = vector [2.0; 2.0; -4.0]
         let transformation = {translate=None; scale=Some {x=2.;y=2.;z=1.}; rotation=None}
-        let transformedVector = (transformVector transformation vec).ToArray()
-        let expected = (vector[4.0;4.0;-4.0]).ToArray()
+        let transformedVector = (transformVector transformation vec).ToSeq()
+        let expected = (vector[4.0;4.0;-4.0]).ToSeq()
 
-        Assert.Equal(expected.[0], transformedVector.[0])
-        Assert.Equal(expected.[1], transformedVector.[1])
-        Assert.Equal(expected.[2], transformedVector.[2])
+        Assert.Equal<seq<float>>(expected, transformedVector)
     
